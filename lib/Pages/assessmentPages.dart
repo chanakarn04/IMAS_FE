695995d7a9a0f56364f0aa5ae5ae2ed7b9814b54
chanakarn04 <57610588+chanakarn04@users.noreply.github.com>
2021-01-08 13:43:51 +0700

    dress up appBar

--- lib/Pages/assessmentPages.dart
+++ lib/Pages/assessmentPages.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../Widget/sideDrawer.dart';
+
 class AssessmentPages extends StatelessWidget {
+  GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldState,
+      endDrawer: SideDrawer(),
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_rounded),
+            onPressed: () => Navigator.of(context).pop()),
+        title: Container(
+          alignment: Alignment.center,
+          child: Text('IMAS'),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.menu_rounded,
+            ),
+            onPressed: () {
+              _scaffoldState.currentState.openEndDrawer();
+            },
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
