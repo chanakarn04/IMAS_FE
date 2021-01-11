761e7f3f24364398c00143b2b3a993a78ad71edd
chanakarn04 <57610588+chanakarn04@users.noreply.github.com>
2021-01-11 22:40:00 +0700

    fix adaptive raised button

--- lib/Widget/AdaptiveRaisedButton.dart
+++ lib/Widget/AdaptiveRaisedButton.dart
@@ -4,23 +4,21 @@ import 'package:flutter/cupertino.dart';
 class AdaptiveRaisedButton extends StatelessWidget {
   final String buttonText;
   final Function handlerFn;
-  final double pctWidth;
   final double pctHeight;
 
-  AdaptiveRaisedButton(
-      this.buttonText, this.pctWidth, this.pctHeight, this.handlerFn);
+  AdaptiveRaisedButton(this.buttonText, this.pctHeight, this.handlerFn);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * pctWidth,
-      height: MediaQuery.of(context).size.height * pctHeight,
-      child: CupertinoButton(
-        onPressed: handlerFn,
-        color: Theme.of(context).primaryColor,
-        child: Text(buttonText),
-        padding: EdgeInsets.all(5),
+    return CupertinoButton(
+      onPressed: handlerFn,
+      color: Theme.of(context).primaryColor,
+      child: Container(
+        padding: EdgeInsets.all(7),
+        height: MediaQuery.of(context).size.height * pctHeight,
+        child: FittedBox(child: Text(buttonText)),
       ),
+      padding: EdgeInsets.all(5),
     );
   }
 }
