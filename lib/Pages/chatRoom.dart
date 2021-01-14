638eb965bf8771daef85fb446b070e5f454a8ea9
chanakarn04 <57610588+chanakarn04@users.noreply.github.com>
2021-01-14 21:57:43 +0700

    add chatroom

---
+++ lib/Pages/chatRoom.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+import '../Widget/textBoxItem.dart';
+
+class Conversation {
+  final int role;
+  final String msg;
+
+  Conversation(
+    @required this.role,
+    @required this.msg,
+  );
+}
+
+class ChatRoom extends StatefulWidget {
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+  final _textController = TextEditingController();
+  final List<Conversation> conversationList = [
+    Conversation(1, 'Hi, How can i help you'),
+    Conversation(0, 'Mr.Harold, I dont feel so well'),
+    Conversation(0, 'I dont like sand'),
+  ];
+
+  _sendConversation() {
+    // print('run here');
+    if (_textController.text.isEmpty) {
+      return;
+    } else {
+      setState(() {
+        conversationList.add(Conversation(0, _textController.text));
+      });
+      _textController.clear();
+      FocusScope.of(context).unfocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(
+      iconTheme: IconThemeData(
+        color: Theme.of(context).primaryColor,
+      ),
+      backgroundColor: Colors.white,
+      leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_rounded),
+          onPressed: () => Navigator.of(context).pop()),
+      title: Row(
+        children: <Widget>[
+          Container(
+            height: 58,
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/doctor.png'),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 1.5,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Text('Doctor Harold')
+        ],
+      ),
+    );
+
+    final textInputBar = Container(
+      alignment: Alignment.bottomCenter,
+      height: 50,
+      width: double.infinity,
+      color: Theme.of(context).primaryColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.image_outlined,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              print('Select image');
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 10,
+                top: 5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Aa',
+                ),
+                controller: _textController,
+                onSubmitted: _sendConversation(),
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.send_rounded,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // print('test test');
+              _sendConversation();
+            },
+          )
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                  appBar.preferredSize.height -
+                  MediaQuery.of(context).padding.top -
+                  50),
+              child: ListView.builder(
+                itemCount: conversationList.length,
+                itemBuilder: (ctx, index) {
+                  return Container(
+                    alignment: conversationList[index].role == 0
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: TextBoxItem(conversationList[index].role,
+                        conversationList[index].msg),
+                  );
+                },
+              ),
+            ),
+            textInputBar,
+          ],
+        ),
+      ),
+    );
+  }
+}
