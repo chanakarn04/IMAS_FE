1f518b0bb43c6d9853551a039b3179391734ba08
chanakarn04 <57610588+chanakarn04@users.noreply.github.com>
2021-02-04 16:07:34 +0700

    edit appearance of widget in login

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -11,47 +11,104 @@ class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,
-            child: Logo(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: 70,
+            bottom: 20,
           ),
-          Container(
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-              controller: usrnTextController,
-            ),
-          ),
-          Container(
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              controller: pswTextController,
-            ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text('Forget Password?'),
-          ),
-          AdaptiveRaisedButton(
-            buttonText: 'LOGIN',
-            handlerFn: () {},
-            height: 30,
-            width: 100,
-          ),
-          Row(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
             children: <Widget>[
-              Text(
-                'Don\u0027t have an account? ',
+              Container(
+                alignment: Alignment.centerRight,
+                child: SizedBox(
+                  height: 130,
+                  child: FittedBox(child: Logo()),
+                ),
               ),
-              Text('Sign Up'),
+              Expanded(
+                child: Container(),
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                  ),
+                  controller: usrnTextController,
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  controller: pswTextController,
+                  onSubmitted: (_) => FocusScope.of(context).unfocus(),
+                  obscureText: true,
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                height: 40,
+                child: TextButton(
+                  onPressed: () {
+                    print('to forget password');
+                  },
+                  child: FittedBox(
+                    child: Text(
+                      'Forget Password?',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 125, 125, 125)),
+                    ),
+                  ),
+                ),
+              ),
+              AdaptiveRaisedButton(
+                buttonText: 'LOGIN',
+                handlerFn: () {
+                  if (usrnTextController.text.isNotEmpty &&
+                      pswTextController.text.isNotEmpty) {
+                    // passing data to verify
+                    print('call login service');
+                    print('user: ${usrnTextController.text}');
+                    print('pswd: ${pswTextController.text}');
+                  }
+                },
+                height: 35,
+                width: 120,
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Don\u0027t have an account? ',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 75, 75, 75),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
