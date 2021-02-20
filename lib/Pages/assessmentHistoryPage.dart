46f1ed2e8b0344801a90acb5b46895cf77b3ede5
chanakarn04 <57610588+chanakarn04@users.noreply.github.com>
2021-02-20 14:33:52 +0700

    finish ass his

--- lib/Pages/assessmentHistoryPage.dart
+++ lib/Pages/assessmentHistoryPage.dart
@@ -1,11 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class AssessmentHistoryPage extends StatelessWidget {
+import './predictionResultPage.dart';
+
+class AssessmentHistoryPage extends StatefulWidget {
   static const routeName = '/assessment history';
+  final String pId = 'p001';
+
+  @override
+  _AssessmentHistoryPageState createState() => _AssessmentHistoryPageState();
+}
+
+class _AssessmentHistoryPageState extends State<AssessmentHistoryPage> {
+  List<Map<String, Object>> items = [
+    {
+      'symptom': 'Headache',
+      'date': DateTime.now(),
+    },
+    {
+      'symptom': 'Forearm pain',
+      'date': DateTime(2020, 12, 10),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+    {
+      'symptom': 'Dizziness',
+      'date': DateTime(2020, 11, 28),
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('ass hst')),
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_rounded),
+            onPressed: () => Navigator.of(context).pop()),
+        title: Container(
+          alignment: Alignment.center,
+          child: Text('Assessment history'),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.menu_rounded,
+              color: Colors.transparent,
+            ),
+            onPressed: null,
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        // color: Colors.teal[200],
+        alignment: Alignment.center,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              margin: (index == 0)
+                  ? EdgeInsets.only(
+                      top: 10,
+                      bottom: 2.5,
+                    )
+                  : (index == items.length - 1)
+                      ? EdgeInsets.only(
+                          top: 2.5,
+                          bottom: 10,
+                        )
+                      : EdgeInsets.symmetric(
+                          vertical: 2.5,
+                        ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2,
+                  color: Theme.of(context).primaryColor,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ListTile(
+                title: Text(
+                  items[index]['symptom'],
+                ),
+                subtitle: Text(
+                  '${DateFormat.yMd().format(items[index]['date'])} ${DateFormat.jm().format(items[index]['date'])}',
+                ),
+                trailing: InkWell(
+                  onTap: () {
+                    Navigator.of(context)
+                        .pushNamed(PredictionResultPage.routeName);
+                  },
+                  child: Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: Theme.of(context).primaryColor,
+                    size: 36,
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: items.length,
+        ),
+      ),
     );
   }
 }
